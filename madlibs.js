b8aa2780593b03914abf7e30e7f6b2426af872f0
Gracelyn Prajna <gracelynprajna@gmail.com>
2022-10-03 21:21:58 -0400

    Update madlibs.js

--- madlibs.js
+++ madlibs.js
@@ -1,1 +1,16 @@
-////test///
+let myAdjective = ['excited', 'angry', 'happy']
+
+let x = (5*3)
+
+let myNouns = ['park', 'cafe', 'train station']
+
+let Random = true
+
+let story = `Today was a very ${myAdjective[2]} day today, but I had tons of TKH homework to do and that made me
+feel super ${myAdjective[0]}. Then I took my ${x} pets out on a walk to the ${myNouns[0]} which made it very ${myAdjective[1]}. 
+I went to ${myNouns[1]} to start doing my homework which was ${Random}. After I finished, I went to the ${myNouns[2]}. It was a 
+very productive day. `
+
+console.log(story)
+
+//my partner was Diana Vargas//
